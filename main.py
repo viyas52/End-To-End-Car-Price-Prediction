ae4bdc5206fba92492989d0d8b2768b88250b6b9
viyas52 <vyas.illusionist@gmail.com>
2024-06-15 22:10:13 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from car_price_pred import logger
+from car_price_pred.pipeline.stage_01_data_ingestion import DataIngestiontrainingPipeline
 
-logger.info("welcome to our custom log")
-
+STAGE_NAME = "data ingestion stage"
+try:
+    logger.info(f'>>>>>>> stage {STAGE_NAME} started <<<<<<<<')
+    obj = DataIngestiontrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>>>>>>>> {STAGE_NAME} completed <<<<<<<<<<< \n\n x=========================x')
+                    
+except Exception as e :
+    logger.exception(e)
+    raise e   
